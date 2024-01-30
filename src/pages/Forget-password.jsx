30d76ecc00e9d6fbdbff913e5a4d899b11c77178
PrashanthSubramani <prashanthsubramani1699@gmail.com>
2024-01-30 19:57:18 +0530

    deployment data changed in fronend

--- src/pages/Forget-password.jsx
+++ src/pages/Forget-password.jsx
@@ -29,7 +29,7 @@ export default function ForgetPassword() {
     const handleSubmit = async (e)=>{
         e.preventDefault();
         try{
-            const { data } = await axios.post("http://localhost:4000/forget-password",{
+            const { data } = await axios.post("https://jwt-login-auth-backend.onrender.com/forget-password",{
                 ...values   
             },{
                 withCredentials:true,
