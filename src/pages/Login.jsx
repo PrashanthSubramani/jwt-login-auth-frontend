b1e62d5b96b87849cf55a92537cdf8eb738a9ea3
PrashanthSubramani <prashanthsubramani1699@gmail.com>
2024-02-01 11:49:03 +0530

    working in cookie react 13

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -34,7 +34,7 @@ export default function Login() {
             const { data } = await axios.post("http://localhost:4000/login",{
                 ...values   
             },{
-                credentials:'include',
+                credentials:true,
             });
 
             if(data){
--- src/pages/Secret.jsx
+++ src/pages/Secret.jsx
@@ -23,7 +23,7 @@ export default function Secret() {
       } else {
         try {
           const response = await axios.post("http://localhost:4000", {}, {
-            credentials: 'include' // Corrected typo
+            credentials:true,
           });
   
           const { data } = response;
